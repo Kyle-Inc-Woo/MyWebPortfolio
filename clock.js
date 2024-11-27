5f0b89c642869372b6943f3503f7644f878bac75
Inchang Woo <143403168+Kyle-Inc-Woo@users.noreply.github.com>
2024-11-27 07:58:42 -0800

    --Updates--

---
+++ clock.js
@@ -0,0 +1,13 @@
+function updateDateTime() {
+    const now = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    const formattedDateTime = now.toLocaleDateString('en-US', options);
+    
+    document.getElementById('dateTimeDisplay').innerText = formattedDateTime;
+ }
+ 
+
+ document.addEventListener('DOMContentLoaded', function() {
+    updateDateTime();
+    setInterval(updateDateTime, 1000);
+ });
